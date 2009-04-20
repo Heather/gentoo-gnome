Heather/gentoo-gnome

commit 033a3748a7dc3e2125eb302d1896c334b2d1885e

Update the gnome meta ebuild for Brasero
modified:   Manifest
modified:   gnome-2.26.0.ebuild

gnome-base/gnome/Manifest
@@ -1,1 +1,1 @@
-EBUILD gnome-2.26.0.ebuild 5423 RMD160 c7a995b8ff1107cd7d75894467ccaf5115fb69c4 SHA1 b23b0e07dcaad8bcb7a7e50cfde1498b2a37a35e SHA256 420674561954f73de91d8bffea4be5e02d785929af46cb225f7c36bd9d722684
+EBUILD gnome-2.26.0.ebuild 5502 RMD160 6c9ba0d42160e10621834d2803fe36d5463bceaa SHA1 48919a0da71fd72a6a481a4b72d733978c7b7f08 SHA256 9ae4581030a71c78211b7cfe3271a647de3eefd1a8c20fe0092dc2c20f6feac7

gnome-base/gnome/gnome-2.26.0.ebuild
@@ -122,8 +122,14 @@ RDEPEND="
 	>=net-analyzer/gnome-netstatus-2.26.0
 	>=net-analyzer/gnome-nettool-2.26.0
 
-	cdr? ( >=gnome-extra/nautilus-cd-burner-2.24.0 )
-	dvdr? ( >=gnome-extra/nautilus-cd-burner-2.24.0 )
+	cdr? (
+		|| (
+			>=app-cdr/brasero-2.26.0
+			>=gnome-extra/nautilus-cd-burner-2.24.0 ) )
+	dvdr? (
+		|| (
+			>=app-cdr/brasero-2.26.0
+			>=gnome-extra/nautilus-cd-burner-2.24.0 ))
 
 	>=gnome-extra/gtkhtml-3.26.0
 	>=mail-client/evolution-2.26.0

commit 37cf79dbba2d4941601efee4d8bd6c85c53df853

gnome-base/gnome: Fix syntax error
Introduced in 033a3748a7dc3e2125eb302d1896c334b2d1885e
Consequentive parenthesis in RDEPEND need a space inbetween.

gnome-base/gnome/Manifest
@@ -1,1 +1,1 @@
-EBUILD gnome-2.26.0.ebuild 5502 RMD160 6c9ba0d42160e10621834d2803fe36d5463bceaa SHA1 48919a0da71fd72a6a481a4b72d733978c7b7f08 SHA256 9ae4581030a71c78211b7cfe3271a647de3eefd1a8c20fe0092dc2c20f6feac7
+EBUILD gnome-2.26.0.ebuild 5503 RMD160 c83cf75bb604ccffc75c4438f878e12b3dba2806 SHA1 701c5bee0cd8cac0986b13da9a3eaac21421cb0d SHA256 f524ccd1591c422d6a2de1ecc832dccf26120e6f2364897a303c1eff8aa33fb1

gnome-base/gnome/gnome-2.26.0.ebuild
@@ -129,7 +129,7 @@ RDEPEND="
 	dvdr? (
 		|| (
 			>=app-cdr/brasero-2.26.0
-			>=gnome-extra/nautilus-cd-burner-2.24.0 ))
+			>=gnome-extra/nautilus-cd-burner-2.24.0 ) )
 
 	>=gnome-extra/gtkhtml-3.26.0
 	>=mail-client/evolution-2.26.0