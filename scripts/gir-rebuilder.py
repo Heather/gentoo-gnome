5ea970fbdfa52f7e9f5a222ad99bfb228b57330f
Nirbheek Chauhan <nirbheek@gentoo.org>
2011-03-24 03:25:21 +0530

    gir-rebuilder.py: add a --force option, use EOutput, fix small bugs
    * Add --force to force rebuilding of all .girs
    * Use EOutput and spinners to display status
    * Fix a bug where an installed package was no longer in portage
    * Use vartree dbapi for *all* API access

--- scripts/gir-rebuilder.py
+++ scripts/gir-rebuilder.py
@@ -17,31 +17,37 @@ from xml.dom import minidom as dom
 
 import portage
 from portage.versions import vercmp
-from portage.output import colorize
+from portage.output import EOutput
+from _emerge.stdout_spinner import stdout_spinner
 
 ###############
 ## Variables ##
 ###############
 quiet = False
+force = False
 root = '/'
 gir_dirs = ['/usr/share/gir-1.0']
 girs = {}
 girversion = ''
 gi = 'gobject-introspection'
 settings = portage.settings
+eoutput = EOutput()
+spinner = stdout_spinner()
 
 ###############
 ## Functions ##
 ###############
 def usage():
-    print """
-gir-rebuilder: Rebuilds gobject-introspection typelibs and girs in the following directories:
+    print """gir-rebuilder: Rebuilds gobject-introspection typelibs and girs in the following directories:
  %s
 
-Usage: %s [ARGUMENTS TO EMERGE]
+Usage: %s [--force] [ARGUMENTS TO EMERGE]
 
 Arguments:
-  All arguments are passed to portage
+  --help            This text
+  --force           Force rebuilding of *all* girs and typelibs
+
+  All other arguments are passed to portage
     """ % ('\n '.join(gir_dirs), sys.argv[0])
 
 def get_version(gir):
@@ -51,26 +57,26 @@ def get_contents(cpv):
     cpv = portage.catsplit(cpv)
     return set(portage.dblink(cpv[0], cpv[1], root, settings).getcontents().keys())
 
-def print_colorize(color, text):
-    print colorize(color, " * ") + text
-
 ################
 ## Parse Args ##
 ################
 if '--help' in sys.argv:
     usage()
     exit(0)
+if '--force' in sys.argv:
+    force = True
+    sys.argv.remove('--force')
 
 ##############
 ## Set vars ##
 ##############
 if os.environ.has_key('ROOT'):
     root = os.environ['ROOT']
-portdb = portage.db[root]["vartree"].dbapi
+vardbapi = portage.vardbapi()
 # Find the latest g-i
 # XXX: Assumes there's only one slot for g-i
-print_colorize("green", "Finding current GIRepository version...")
-gi = portdb.match(gi)[0]
+eoutput.ebegin("Finding current GIRepository version")
+gi = vardbapi.match(gi)[0]
 for each in get_contents(gi):
     # Find GIRepository-$ver.gir, and get the internal version
     if re.match('.*GIRepository[^/]+\.gir$', each):
@@ -79,12 +85,16 @@ for each in get_contents(gi):
 else:
     raise Exception("GIRepository .gir not found")
 girversion = get_version(girepository)
-print_colorize("green", "Current GIRepository version is " + girversion)
+eoutput.eend(0)
+eoutput.einfo("Current GIRepository version is " + girversion)
 
 ##########
 ## Work ##
 ##########
-print_colorize("green", "Finding broken GIR files...")
+if force:
+    eoutput.ebegin("Finding GIR files")
+else:
+    eoutput.ebegin("Finding broken GIR files")
 files_list = set()
 for dir in gir_dirs:
     # Walk the gir directories to find files
@@ -92,38 +102,45 @@ for dir in gir_dirs:
         for f in files:
             if not f.endswith('.gir'):
                 continue
+            if force:
+                files_list.add(osp.join(path, f))
+                continue
+            spinner.update()
             # Get the .gir version
             version = get_version(osp.join(path, f))
             # If not the same version as GIRepository.gir, rebuild it
             if vercmp(girversion, version) != 0:
-                print_colorize("yellow", "GIR file to be rebuilt: " + \
+                eoutput.ewarn("GIR file to be rebuilt: " + \
                                          osp.join(path, f))
                 files_list.add(osp.join(path, f))
+eoutput.eend(0)
 
 # FIXME: Doesn't warn if it was unable to assign a file to a package
 rebuild_list = set()
 if files_list:
-    print_colorize("green", "Assigning files to packages...")
+    eoutput.ebegin("Assigning files to packages")
     files_assigned = set()
-    for cpv in portage.db[root]["vartree"].dbapi.cpv_all():
+    for cpv in vardbapi.cpv_all():
+        spinner.update()
         # If some of the files of this package are in the gir file list
         files_owned = get_contents(cpv).intersection(files_list)
         if files_owned:
-            files_assigned.add(files_owned)
-            slot = portage.portdb.aux_get(cpv, ['SLOT'])[0]
+            files_assigned.update(files_owned)
+            slot = vardbapi.aux_get(cpv, ['SLOT'])[0]
             # We strip the version, but maintain the slot (same as revdep-rebuild)
             rebuild_list.add(portage.pkgsplit(cpv)[0]+':'+slot)
     files_unassigned = files_list.symmetric_difference(files_assigned)
     if files_unassigned:
         for file in files_unassigned:
-            print_colorize("yellow", "Unable to assign file to package: " + \
+            eoutput.ewarn("Unable to assign file to package: " + \
                                      osp.join(path, file))
+eoutput.eend(0)
 
 if files_list and rebuild_list:
-    print_colorize("green", "All done, starting rebuild...")
-    command = "emerge -1 " + '\n'.join(sys.argv[1:]+list(rebuild_list))
+    eoutput.einfo("All done, starting rebuild")
+    command = "emerge --oneshot " + ' \\\n'.join(sys.argv[1:]+list(rebuild_list))
     print command
     # Run emerge
-    subprocess.check_call(command, shell=True)
+    subprocess.call(command, shell=True)
 else:
-    print_colorize("green", "Everything seems to be in order, nothing to be rebuilt")
+    eoutput.einfo("Everything seems to be in order, nothing to be rebuilt")
