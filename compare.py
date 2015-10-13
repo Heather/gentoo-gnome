67b146b194ebba0b44af50ac89e5807d3926b96a
Heather <Heather@live.ru>
2015-10-13 14:23:36 +0400

    script which cleans up packages that are already updated in overlay or in main tree

---
+++ compare.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+__author__ = 'Heather'
+
+import os
+import sys
+import glob
+import re
+import shutil
+
+def regex_key(key):
+  """Traditional, regular-expression-based nat-sort key."""
+  convert = lambda text: int(text) if text.isdigit() else text.lower()
+  return [convert(c) for c in re.split('([0-9]+)', key)]
+
+def nat_cmp(a, b):
+  return (regex_key(a) >= regex_key(b))
+
+if len(sys.argv) < 2:
+  print("pass overlay path to compare with as first argument")
+  sys.exit()
+overlay = sys.argv[1]
+for root, dirs, files in os.walk('.', topdown=True, followlinks=False):
+  dirs[:] = list(filter(lambda x: not x in [".git", "profiles", "metadata", "files"], dirs))
+  if dirs and root != ".":
+    for dir in dirs:
+      realpath = os.path.join(root, dir)
+      fullpath = realpath[2:]
+      chkpath = os.path.join(overlay, fullpath)
+      if os.path.isdir(chkpath):
+        efullpath = list(filter(lambda x: not x.endswith("-9999.ebuild"), glob.glob(os.path.join(realpath, "*.ebuild"))))
+        echkpath = list(filter(lambda x: not x.endswith("-9999.ebuild"), glob.glob(os.path.join(chkpath, "*.ebuild"))))
+        if efullpath and echkpath:
+          enamesf = map(lambda x: os.path.splitext(os.path.basename(x))[0], efullpath)
+          enamesc = map(lambda x: os.path.splitext(os.path.basename(x))[0], echkpath)
+          maxf = max(enamesf)
+          maxc = max(enamesc)
+          if nat_cmp(maxc, maxf):
+            print(maxc, " >= ", maxf)
+            print("removing ", realpath)
+            shutil.rmtree(realpath)
